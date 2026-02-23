a40e38ab065155ba8dbbd5c326b2525e071f22de
dkwang62 <korealocks@gmail.com>
2026-02-24 01:39:16 +0800

    Add files via upload

---
+++ pages/02_Spanish.py
@@ -0,0 +1,4 @@
+from unified_runner import render_app_switcher, run_legacy_app
+
+run_legacy_app("Spanish", "app.py")
+render_app_switcher("Spanish")
---
+++ pages/03_MVC.py
@@ -0,0 +1,4 @@
+from unified_runner import render_app_switcher, run_legacy_app
+
+run_legacy_app("MVC", "app.py")
+render_app_switcher("MVC")
